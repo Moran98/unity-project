20bb32db45ef40f74cd19ca5832ee0225ef3b04e
Aaron Moran <aaronmorann@protonmail.com>
2020-04-07 16:43:17 +0100

    collectables/speeds

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Variables
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Text countText;
+    public Text win;
 
     Vector2 movement;
     Vector2 mousePos;
 
     private Camera gameCamera;
+    private int count;
 
     [SerializeField] private float speed = 20f;
 
@@ -21,6 +26,9 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        count=0;
+        win.text="";
+        setCountText();
     }
 
     void Update()
@@ -40,13 +48,22 @@ public class PlayerMovement : MonoBehaviour
 
         rb.position = new Vector2(xValue, yValue);
     }
-}
-/*    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector2 lookDirection = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+    // Collectable items
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("PickUp")){
+            // Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            count= count+1;
+            setCountText();     
+}
     }
-}*/
+
+    //Score counter and winner message
+    void setCountText(){
+        countText.text="SCORE : "+count.ToString();
+        if(count>=4){
+            win.text="YOU WIN!";
+        }
+    }
+}
